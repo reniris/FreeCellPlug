488bafcc2eeb71d3ec76061905dcc5e7644441dc
reniris <4769180+reniris@users.noreply.github.com>
2020-03-28 02:28:04 +0900

    initial commit

---
+++ Source/FreeCellPlug/FreeCellPlug/Models/Model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Livet;
+
+namespace FreeCellPlug.Models
+{
+    public class Model : NotificationObject
+    {
+    }
+}
---
+++ Source/FreeCellPlug/FreeCellPlug/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+using Livet;
+using Livet.Commands;
+using Livet.Messaging;
+using Livet.Messaging.IO;
+using Livet.EventListeners;
+using Livet.Messaging.Windows;
+
+using FreeCellPlug.Models;
+
+namespace FreeCellPlug.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        // Some useful code snippets for ViewModel are defined as l*(llcom, llcomn, lvcomm, lsprop, etc...).
+        public void Initialize()
+        {
+        }
+    }
+}
